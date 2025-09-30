ce34a0122832b0afcdc881e1a43318173b8b541b
615haseebansari-dotcom <615haseebansari@gmail.com>
2025-09-30 23:10:35 +0500

    Delete src directory

--- src/hooks/useSEO.ts
+++
@@ -1,9 +0,0 @@
-import { useLocation } from 'react-router-dom';
-import { getSEOData } from '../utils/seo';
-
-export function useSEO() {
-  const location = useLocation();
-  const seoData = getSEOData(location.pathname);
-  
-  return seoData;
-}
